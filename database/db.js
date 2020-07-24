bdeb1c3b871cb8e04336b6454dfb7c6aae1b920f
Henry Wang <henry.h.wang93@gmail.com>
2020-07-24 01:08:47 -0400

    update Dockerfiles

--- database/db.js
+++ database/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: 'db',
+  port: '3306',
+  user: 'mysql',
+  password: 'password',
   database: 'sdcqa',
 });
 
