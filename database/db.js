a7e7f74b3d3598abc0ac30b40340612b29b30766
Henry Wang <henry.h.wang93@gmail.com>
2020-07-28 00:58:44 -0400

    update module for deployment

--- database/db.js
+++ database/db.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: '18.144.10.13',
+  port: '3306',
+  user: 'root',
+  password: process.env.DB_PASS,
+  database: 'sdcqa',
 });
 
 connection.connect((err) => {
   if (err) {
     console.log('failed to connect to MySQL from db, err: ', err);
-    // console.log('process.env: ', process.env);
+    console.log('process.env: ', process.env);
   } else {
     console.log('Connected to the MySQL server.');
   }
