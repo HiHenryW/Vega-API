e73017e93a0a3a585e68a6f63375ad3022726b55
Henry Wang <henry.h.wang93@gmail.com>
2020-08-03 23:34:29 -0400

    update deployment version

--- database/db.js
+++ database/db.js
@@ -4,8 +4,8 @@ const connection = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  password: process.env.DB_PASS,
+  database: process.env.MYSQL_DB,
 });
 
 connection.connect((err) => {
